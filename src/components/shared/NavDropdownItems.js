e465afb1fd091dd0ea44ccf7d5717324c6a2cbf1
Barry Rollan <barryrollan@macbook-pro.lan>
2020-12-17 08:35:37 -0500

    changed nav operation so that dropdown drops directly from menu, as opposed to a sub-dropdown, on medium to small screens

---
+++ src/components/shared/NavDropdownItems.js
@@ -0,0 +1,71 @@
+import React, { useContext } from 'react';
+import { UserContext } from '../../App';
+import { LoginContext } from '../../App';
+import { Link } from 'react-router-dom';
+import Zillow from '../../assets/zillow.png';
+import styles from './NavBar.module.scss';
+
+export default function NavDropdownItems({ collapseNavbar }) {
+  const thisUser = useContext(UserContext);
+  const setLoginPopup = useContext(LoginContext);
+  return (
+    <div>
+      <Link
+        to="/"
+        className="dropdown-item"
+        onClick={() => collapseNavbar()}
+        style={{ display: thisUser ? 'inherit' : 'none' }}
+      >
+        Discussion
+      </Link>
+      <Link
+        to="/calendar"
+        className="dropdown-item"
+        onClick={() => collapseNavbar()}
+        style={{ display: thisUser ? 'inherit' : 'none' }}
+      >
+        Calendar
+      </Link>
+      <Link
+        to="/directory"
+        className="dropdown-item"
+        onClick={() => collapseNavbar()}
+        style={{ display: thisUser ? 'inherit' : 'none' }}
+      >
+        Directory
+      </Link>
+      <a
+        href="https://www.zillow.com/homes/for_sale/?searchQueryState=%7B%22usersSearchTerm%22%3A%2230067%22%2C%22mapBounds%22%3A%7B%22west%22%3A-84.43712215325927%2C%22east%22%3A-84.42454795739745%2C%22south%22%3A33.94546877093187%2C%22north%22%3A33.95742995262041%7D%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22ah%22%3A%7B%22value%22%3Atrue%7D%7D%2C%22isListVisible%22%3Atrue%2C%22mapZoom%22%3A16%2C%22customRegionId%22%3A%2275aa25f873X1-CReejk073xunpa_wtbvq%22%7D"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="dropdown-item"
+        onClick={() => collapseNavbar()}
+      >
+        Homes by <img src={Zillow} alt="Zillow" className={styles.zillow} />
+      </a>
+      <Link
+        to="/payDues"
+        className="dropdown-item"
+        onClick={() => collapseNavbar()}
+        style={{ display: thisUser ? 'inherit' : 'none' }}
+      >
+        Pay Your Dues
+      </Link>
+      {/* <NavDropdown.Divider /> */}
+      <Link
+        to="/referrals"
+        className="dropdown-item"
+        onClick={() => collapseNavbar()}
+      >
+        Referrals
+      </Link>
+      <div
+        className="dropdown-item"
+        style={{ display: thisUser ? 'none' : 'inherit' }}
+        onClick={() => setLoginPopup.showLoginPopup()}
+      >
+        Login For More
+      </div>
+    </div>
+  );
+}
--- src/components/shared/Navbar.js
+++ src/components/shared/Navbar.js
@@ -1,9 +1,7 @@
 import React, { useContext } from 'react';
-import Zillow from '../../assets/zillow.png';
+import NavDropdownItems from './NavDropdownItems';
 import { UserContext } from '../../App';
 import { LoginContext } from '../../App';
-import { Client } from '../../api/sanityClient';
-import imageUrlBuilder from '@sanity/image-url';
 import { Navbar, Nav, NavDropdown, Dropdown } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import { useHistory } from 'react-router-dom';
@@ -16,11 +14,6 @@ const NavBar = () => {
   const thisUser = useContext(UserContext);
   const setLoginPopup = useContext(LoginContext);
 
-  const builder = imageUrlBuilder(Client);
-
-  const urlFor = (source) => {
-    return builder.image(source);
-  };
   const logInOut = () => {
     if (thisUser) {
       db.signOut();
@@ -28,7 +21,6 @@ const NavBar = () => {
       history.push('/');
     }
     if (!thisUser) {
-      // setShowLogin(true);
       setLoginPopup.showLoginPopup();
     }
   };
@@ -58,11 +50,7 @@ const NavBar = () => {
           </Dropdown.Toggle>
 
           <Dropdown.Menu>
-            <Dropdown.Item
-              id="logBtn"
-              // className={styles.logoutBtn}
-              onClick={() => logInOut()}
-            >
+            <Dropdown.Item id="logBtn" onClick={() => logInOut()}>
               {thisUser ? 'Logout' : 'Login or Sign Up'}
             </Dropdown.Item>
             <Link
@@ -90,78 +78,22 @@ const NavBar = () => {
           </span>
         </Nav>
         <Nav>
-          {/* <Link
-            to="/"
-            className={[`nav-link ${styles.navLink}`]}
-            onClick={() => collapseNavbar()}
-          >
-            Home
-          </Link> */}
-
           <NavDropdown
             title="The Neighborhood"
             id="collasible-nav-dropdown"
-            className={styles.navLink}
+            className={[`${styles.navLink} ${styles.subDrop}`]}
           >
-            {/* <NavDropdown.Item> */}
-            <Link
-              to="/"
-              className="dropdown-item"
-              onClick={() => collapseNavbar()}
-              style={{ display: thisUser ? 'inherit' : 'none' }}
-            >
-              Discussion
-            </Link>
-            <Link
-              to="/calendar"
-              className="dropdown-item"
-              onClick={() => collapseNavbar()}
-              style={{ display: thisUser ? 'inherit' : 'none' }}
-            >
-              Calendar
-            </Link>
-            <Link
-              to="/directory"
-              className="dropdown-item"
-              onClick={() => collapseNavbar()}
-              style={{ display: thisUser ? 'inherit' : 'none' }}
-            >
-              Directory
-            </Link>
-            <a
-              href="https://www.zillow.com/homes/for_sale/?searchQueryState=%7B%22usersSearchTerm%22%3A%2230067%22%2C%22mapBounds%22%3A%7B%22west%22%3A-84.43712215325927%2C%22east%22%3A-84.42454795739745%2C%22south%22%3A33.94546877093187%2C%22north%22%3A33.95742995262041%7D%2C%22isMapVisible%22%3Atrue%2C%22filterState%22%3A%7B%22ah%22%3A%7B%22value%22%3Atrue%7D%7D%2C%22isListVisible%22%3Atrue%2C%22mapZoom%22%3A16%2C%22customRegionId%22%3A%2275aa25f873X1-CReejk073xunpa_wtbvq%22%7D"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="dropdown-item"
-              onClick={() => collapseNavbar()}
-            >
-              Homes by{' '}
-              <img src={Zillow} alt="Zillow" className={styles.zillow} />
-            </a>
-            <Link
-              to="/payDues"
-              className="dropdown-item"
-              onClick={() => collapseNavbar()}
-              style={{ display: thisUser ? 'inherit' : 'none' }}
-            >
-              Pay Your Dues
-            </Link>
-            {/* <NavDropdown.Divider /> */}
-            <Link
-              to="/referrals"
-              className="dropdown-item"
-              onClick={() => collapseNavbar()}
-            >
-              Referrals
-            </Link>
-            <div
-              className="dropdown-item"
-              style={{ display: thisUser ? 'none' : 'inherit' }}
-              onClick={() => setLoginPopup.showLoginPopup()}
-            >
-              Login For More
-            </div>
+            <NavDropdownItems
+              collapseNavbar={collapseNavbar}
+              logInOut={logInOut}
+            />
           </NavDropdown>
+          <div className={styles.directDrop}>
+            <NavDropdownItems
+              collapseNavbar={collapseNavbar}
+              logInOut={logInOut}
+            />
+          </div>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
