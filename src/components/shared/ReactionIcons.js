ca6f055f1231b6d7e445f0f1f9b3b9acdd28436c
Barry Rollan <barryrollan@macbook-pro.lan>
2020-12-29 16:22:35 -0500

    reaction icons working properly, now need to reflect changes in ResponseAccordion

--- src/components/shared/ReactionIcons.js
+++ src/components/shared/ReactionIcons.js
@@ -1,88 +1,99 @@
-import React, { useReducer, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { UserContext } from '../../App';
+import { messagesCollection } from '../../firestore/index';
 import { reactions } from '../../data/reactions';
 import $ from 'jquery';
 import styles from './ReactionIcons.module.scss';
 
-const union = require('lodash.union');
 const remove = require('lodash.remove');
-
-const reducer = (state, action) => {
-  let arrayName = action.payload.array;
-  let userName = [`${action.payload.name}`];
-  switch (action.type) {
-    case 'add':
-      const mArray = union(state[arrayName], userName);
-      console.log(mArray);
-      return {
-        ...state,
-        [arrayName]: mArray,
-      };
-    case 'remove':
-      console.log('case remove triggered');
-      return {
-        ...state,
-        [arrayName]: remove([arrayName], (name) => {
-          return name !== `${action.payload}`;
-        }),
-      };
-    default:
-      return { ...state };
-  }
-};
 
 const ReactionIcons = ({ m }) => {
   const thisUser = useContext(UserContext);
-  let likedBy = m.likedBy ? [...m.likedBy] : [];
-  let lovedBy = m.lovedBy ? [...m.lovedBy] : [];
-  let criedBy = m.criedBy ? [...m.criedBy] : [];
-  let laughedBy = m.laughedBy ? [...m.laughedBy] : [];
-  let mergedBys = [...likedBy, ...lovedBy, ...criedBy, ...laughedBy];
-  let byObj = { likedBy, lovedBy, criedBy, laughedBy };
-
-  const [state, dispatch] = useReducer(reducer, { ...byObj });
-
-  ////////lodash practice
-  // let arr = ['Barry', 'Lucius', 'Beth', 'Harry', 'Lucius', 'Barry'];
-  // arr = remove(arr, (name) => {
-  //   return name !== 'Harry';
-  // }); //['Barry', 'Lucius', 'Beth', 'Lucius', 'Barry']
-  // arr = union(arr, ['Chuck', 'Sandra', 'Chuck']);
-  // console.log(arr); //  }); //['Barry', 'Lucius', 'Beth', 'Chuck', 'Sandra']
-
-  const affectReaction = () => {};
   const me = thisUser.name;
-  let iAmHere = false;
-  if (mergedBys.includes(me)) {
-    iAmHere = true;
+  const id = m.id;
+
+  const [state, setState] = useState({});
+  let likedBy = state.likedBy ?? [];
+  let lovedBy = state.lovedBy ?? [];
+  let criedBy = state.criedBy ?? [];
+  let laughedBy = state.laughedBy ?? [];
+
+  let iHaveReacted = false;
+  if (
+    likedBy.includes(me) ||
+    lovedBy.includes(me) ||
+    criedBy.includes(me) ||
+    laughedBy.includes(me)
+  ) {
+    iHaveReacted = true;
   }
+
+  const affectReaction = (addRemove, array) => {
+    let newData = {};
+    const currentArray = state[array] ?? []; //state.likedBy, i.e.
+    let newArray;
+    switch (addRemove) {
+      case 'add':
+        newArray = [...currentArray, me];
+        newData = { ...state, [array]: [...newArray] };
+        console.log(newData);
+        break;
+      case 'remove':
+        newArray = remove(currentArray, (name) => {
+          return name != me;
+        });
+        newData = { ...state, [array]: [...newArray] };
+        console.log(newData);
+        break;
+      default:
+        console.log(state);
+    }
+    messagesCollection.doc(id).set(newData);
+    setState(newData);
+  };
+
+  useEffect(() => {
+    let mounted = true;
+    let thisMessage = {};
+    messagesCollection.doc(id).onSnapshot((doc) => {
+      try {
+        const changeData = { ...doc.data() };
+        thisMessage = { ...changeData };
+      } finally {
+        if (mounted) {
+          setState({ ...thisMessage });
+        }
+      }
+    });
+    const unsubscribe = messagesCollection.onSnapshot(function () {});
+
+    return () => {
+      mounted = false;
+      unsubscribe();
+    };
+  }, [id]);
   return (
     <div className={styles.iconRow}>
       {reactions.map((icon) => {
+        const iconArray = state[icon.array] ?? [];
         return (
           <i
-            key={`${icon.title}Of${m.id}`}
-            id={`${icon.title}Of${m.id}`}
+            key={`${icon.title}Of${id}`}
+            id={`${icon.title}Of${id}`}
             //if reaction array (i.e. likedBy) includes me, then the first click of this button should decrease the likes by 1 and remove "me" from the array (unlike), if me is not already there, then increase the number and add me to the array
-            onClick={() => {
-              dispatch({
-                type:
-                  m[`${icon.array}`] && m[`${icon.array}`].includes(me)
-                    ? 'remove'
-                    : 'add',
-
-                payload: { name: me, array: icon.array },
-              });
-              setTimeout(() => console.log('new state: ', state), 2000);
-            }}
-            className={[`${icon.fontawesome} ${styles.icon} byIcon`]}
-            style={
-              !iAmHere
-                ? { color: icon.color } //if not iAmHere, all icons displayed and grayed out
-                : m[`${icon.array}`] && m[`${icon.array}`].includes(me) // if iAmHere and  this reaction array (i.e. likedBy) includes me
-                ? { display: 'block' } // color it
-                : { display: 'none' } //otherwise (if iAmHere, but not in this array, don't display me)
+            onClick={() =>
+              affectReaction(iHaveReacted ? 'remove' : 'add', icon.array)
             }
+            className={[`${icon.fontawesome} ${styles.icon} byIcon`]}
+            style={{
+              color: icon.color,
+              opacity: iHaveReacted ? 1 : 0.6,
+              display: !iHaveReacted
+                ? 'block' //if I have not reacted, display all reation options
+                : iconArray.includes(me) // if iHaveReacted (in one array) and this particular reaction array (i.e. likedBy, etc.) includes me
+                ? 'block' // show it
+                : 'none', //otherwise (if iHaveReacted, but not in this array, don't display me)
+            }}
           ></i>
         );
       })}
