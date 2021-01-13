fe3d84a46568b85dfb5668a2edee3be317e06a93
Barry Rollan <barryrollan@macbook-pro.lan>
2021-01-13 17:50:12 -0500

    need to see whether functions/index.js is visible

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,35 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const nodemailer = require('nodemailer');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+const gmailUser = process.env.REACT_APP_GMAIL_EMAIL;
+const gmailPass = process.env.REACT_APP_GMAIL_PASSWORD;
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: gmailUser,
+    pass: gmailPass,
+  },
+});
+
+exports.sendEmail = functions.firestore
+  .document('messages/{msgId}')
+  .onCreate((snapshot, context) => {
+    const mailOptions = {
+      from: 'blackland.ridge.notifications@gmail.com',
+      to: 'steve@gmail.com',
+      subject: 'Sending Email using Node.js',
+      html: `<h3>New message from ${snapshot.data().name}</h3>`,
+    };
+
+    return transporter.sendMail(mailOptions, (error, data) => {
+      if (error) {
+        console.log(error);
+        return false;
+      }
+      console.log('Email sent: ' + data.response);
+    });
+  });
