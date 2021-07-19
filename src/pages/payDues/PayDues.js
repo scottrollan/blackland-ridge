d51b042af70b7bd097d9342e45ef34bc92bd74cd
Scott Rollan <b.scott.rollan@gmail.com>
2021-07-19 11:21:39 -0400

    added Pay Dues page with paypal link--Venmo link may be to come

--- src/pages/payDues/PayDues.js
+++ src/pages/payDues/PayDues.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import QuickButtons from '../../components/shared/QuickButtons';
 import Footer from '../../components/shared/Footer';
+import PaypalLogo from '../../assets/paypal.png';
 import styles from './PayDues.module.scss';
 
 export default function PayDues() {
@@ -8,7 +9,32 @@ export default function PayDues() {
     <>
       <QuickButtons />
       <div className={styles.payDues}>
-        <h3>Page Coming Soon... maybe.</h3>
+        <h3>Pay Your $70 Annual Dues</h3>
+        <div className={styles.body}>
+          <div className={styles.paragraph}>
+            <a
+              href="https://www.paypal.me/BlacklandridgeGC"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={styles.link}
+            >
+              <div className={styles.paypalDiv}>
+                <p>Pay here with </p>
+                <img
+                  src="https://www.paypalobjects.com/webstatic/en_US/i/buttons/PP_logo_h_150x38.png"
+                  alt="PayPal"
+                />
+              </div>
+            </a>
+          </div>
+
+          <div className={styles.paragraph}>
+            Or you can write check, payable to{' '}
+            <b>Blackland Ridge Garden Club</b>, and drop it off at the home of
+            Kim Timson, 94 Lakeshore Circle. Just place your check in the
+            mailbox.
+          </div>
+        </div>
         <br />
       </div>
       <Footer />
